205482d3de3538df47e204e43826a0f6cdb5cee7
Michael T Roth <michael.roth@techpath.com>
2014-03-23 16:01:06 -0700

    Initial commit all project files and appenders should be in this commit.

---
+++ SignalRAppender/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("SignalRAppender")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SignalRAppender")]
+[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("ae1f5d95-724b-4703-81c8-fb808b7332d5")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ SignalRGateway/Hubs/EventHub.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using System.Threading.Tasks;
+using System.Collections.Concurrent;
+
+namespace SignalRGateway.Hubs
+{
+    public class EventHub : Hub
+    {
+        public async Task RegisterApplication(string guid)
+        {
+            await Groups.Add(Context.ConnectionId, guid);
+            await Clients.Group(guid).ClientUpdate("Application Connected and is sending events!");
+        }
+
+        public async Task RegisterWatcher(string guidWatching)
+        {
+            string message = string.Format("Team Member is watching");
+            await Groups.Add(Context.ConnectionId, guidWatching);
+            await Clients.Group(guidWatching).ClientUpdate(message);
+        }
+
+        
+
+        public async Task SendUpdate(string guid, string message, string type)
+        {
+            await this.Clients.Group(guid).LogMessage(message, type);
+        }
+
+
+    }
+}
